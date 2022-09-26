a6d3032fdf1c0edce51bff9f6a866ec9e0b569c2
Nandmin <andras78.nemeth@gmail.com>
2022-09-26 15:59:27 +0200

    hambuerger-menu-angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TotemComponent } from './totem/totem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'totem', component: TotemComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
